361fbbdb60bbb0b6331c1347ffffcae0df41fc1b
Thulhid <153322849+Thulhid@users.noreply.github.com>
2024-10-31 03:02:10 +0530

    Add new functionality for handling perform display and deletion features, with local storage support

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const inputCadence = document.querySelector(".form__input--cadence");
 const inputDescription = document.querySelector(".form__input--description");
 const inputTitle = document.querySelector(".form__input--title");
 const inputType = document.querySelector(".form__input--type");
-class Performer {
+const btnDeleteAll = document.querySelector(".delete_all_btn");
+class Perform {
   date = new Date();
   id = (Date.now() + "").slice(-10);
   constructor(coords, title, duration) {
@@ -30,7 +31,7 @@ class Performer {
 // x._setDescription();
 // console.log(x);
 
-class Working extends Performer {
+class Working extends Perform {
   type = "working";
   constructor(coords, title, duration, description) {
     super(coords, title, duration);
@@ -39,7 +40,7 @@ class Working extends Performer {
   }
 }
 
-class Running extends Performer {
+class Running extends Perform {
   type = "running";
   constructor(coords, title, duration, distance, cadence) {
     super(coords, title, duration);
@@ -54,7 +55,6 @@ class Running extends Performer {
     return this.pace;
   }
 }
-
 class App {
   #map;
   #performs = [];
@@ -63,6 +63,7 @@ class App {
     this._getPosition();
     form.addEventListener("submit", this._newPerform.bind(this));
     inputType.addEventListener("change", this._toggleFields);
+    this._getLocalStorage();
   }
   _getPosition() {
     if (navigator.geolocation) {
@@ -75,6 +76,7 @@ class App {
     }
   }
   _loadMap(position) {
+    console.log(position);
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
@@ -88,16 +90,19 @@ class App {
     }).addTo(this.#map);
     //Note: call _showForm
     this.#map.on("click", this._showForm.bind(this));
+    this.#performs.forEach((perform) => this._renderPerformMarker(perform));
+    if (this.#performs.length === 0) return;
+    this._displayDeleteAll();
   }
 
   _toggleFields() {
+    console.dir(inputCadence);
     inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
     inputDistance.closest(".form__row").classList.toggle("form__row--hidden");
     inputDescription
       .closest(".form__row")
       .classList.toggle("form__row--hidden");
   }
-
   _showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove("hidden");
@@ -118,7 +123,6 @@ class App {
     const validInputs = (...inputs) =>
       inputs.every((inp) => Number.isFinite(inp));
     const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
-
     //Info: get data from form
     const type = inputType.value;
     const duration = +inputDuration.value;
@@ -132,9 +136,7 @@ class App {
         return window.alert("Input have to be positive numbers!");
       perform = new Working([lat, lng], title, duration, description);
     }
-
     //Info: if running, running object
-
     if (type === "running") {
       const distance = +inputDistance.value;
       const cadence = +inputCadence.value;
@@ -149,19 +151,23 @@ class App {
     //Info: add new object to performs array
     this.#performs.push(perform);
     //Info: render perform on map as a marker
-    this._renderWorkoutMarker(perform);
+    this._renderPerformMarker(perform);
     //Info: render perform on list
+    this._renderPerform(perform);
     //Info: hide the form + clear input fields
     this._hideForm();
     //Info: Set Local storage to the all performs
+    this._localStorage();
+    //Info: Display delete all button
+    this._displayDeleteAll();
   }
-  _renderWorkoutMarker(performer) {
+  _renderPerformMarker(performer) {
     L.marker(performer.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
           maxWidth: 250,
-          minWidth: 100,
+          minWidth: 150,
           autoClose: false,
           closeOnClick: false,
           className: `${performer.type}-popup`,
@@ -173,6 +179,64 @@ class App {
       .openPopup();
     console.log(this.#performs);
   }
-}
+  _renderPerform(perform) {
+    let html = `
+    <li class="perform perform--${perform.type}" data-id="${perform.id}">
+          <h2 class="perform__title">${perform.info}</h2>
+          <div class="perform__details">
+            <span class="perform__icon">${
+              perform.type === "running" ? "🏃‍♂️" : "👨‍💻"
+            }</span>
+            <span class="perform__value">${perform.title}</span>
+          </div>
+          <div class="perform__details">
+            <span class="perform__icon">⏱</span>
+            <span class="perform__value">${perform.duration}</span>
+            <span class="perform__unit">min</span>
+          </div>
+    `;
+    if (perform.type === "working") {
+      const shortedDescription = perform.description.split(" ");
+      const [value1, value2] = shortedDescription;
+      console.log(shortedDescription);
 
+      html += `<div class="perform__details">
+      <span class="perform__icon">📝</span>
+       <span class="perform__value">${value1} ${value2}...</span>
+      </div> </li>`;
+    }
+    if (perform.type === "running")
+      html += `
+      <div class="perform__details">
+    <span class="perform__icon">🦶</span>
+     <span class="perform__value">${perform.cadence}</span>
+      <span class="perform__unit">spm</span>
+      </div>
+      <div class="perform__details">
+    <span class="perform__icon">🏁</span>
+     <span class="perform__value">${perform.distance}</span>
+      <span class="perform__unit">km</span>
+      </div>
+      </li>
+    `;
+    btnDeleteAll.insertAdjacentHTML("afterend", html);
+  }
+  _localStorage() {
+    window.localStorage.setItem("perform", JSON.stringify(this.#performs));
+  }
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("perform"));
+    if (!data) return;
+    this.#performs = data;
+    this.#performs.forEach((perform) => this._renderPerform(perform));
+  }
+  reset() {
+    localStorage.removeItem("perform");
+    window.location.reload();
+  }
+  _displayDeleteAll() {
+    btnDeleteAll.classList.remove("hidden");
+    btnDeleteAll.addEventListener("click", this.reset);
+  }
+}
 const app = new App();
